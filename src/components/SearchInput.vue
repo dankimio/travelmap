94d721f79004b74b0bf2a73590b5bd9ba44c056e
Dan Kim <itsdanya@gmail.com>
2021-03-02 16:03:48 +0300

    Add SearchInput component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <vue-autosuggest
+      :suggestions="[{data:['gb','ru']}]"
+    >
+      <template slot-scope="{suggestion}">
+        <span class="my-suggestion-item">{{suggestion.item}}</span>
+      </template>
+    </vue-autosuggest>
+  </div>
+</template>
+
+<script>
+import { VueAutosuggest } from 'vue-autosuggest';
+
+export default {
+  components: { VueAutosuggest }
+}
+</script>
+
+<style>
+
+</style>
