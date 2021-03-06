41f81a0fd308144bb735feb57ed194c990b61ee8
Dan Kim <itsdanya@gmail.com>
2021-03-07 00:16:02 +0300

    CountryLabel: remove border

--- src/components/CountryLabel.vue
+++ src/components/CountryLabel.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="px-4 py-1 mr-1 mb-2 inline-block
-    bg-gray-100 border rounded-lg text-xs md:text-sm
+    bg-gray-200 rounded-lg text-xs md:text-sm
     hover:bg-gray-800 hover:text-white hover:border-gray-800
     cursor-pointer"
     @click="onClick"
