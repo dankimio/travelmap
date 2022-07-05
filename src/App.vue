d43594c145c84d733e7b41572b91f8ba6fa1b1d3
Dan Kim <itsdanya@gmail.com>
2022-07-06 00:14:13 +0300

    Update header styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <div class="container mx-auto px-4 py-8">
+    <div class="container mx-auto px-8 py-8">
       <h1
-        class="text-3xl font-bold mb-4 font-serif text-gray-900
-          md:text-center md:text-4xl md:mb-8"
+        class="text-3xl font-bold mb-4 text-gray-900
+          md:text-4xl md:mb-8"
       >
-        Dan's Travel Map 🗺
+        Dan's Travel Map
       </h1>
 
       <Map class="mb-10" />
